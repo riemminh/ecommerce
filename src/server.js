669b6a9fc5415bf3465087030fd9ac59273141c9
riem-pham <riem-pham@kitting-users-MacBook-Pro.local>
2019-07-25 15:57:28 +0700

    make it better2

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "front_end", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../front_end", "build", "index.html"));
   });
 }
 
