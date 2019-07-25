7d2982843457169ce7e51206b2845561e9cace05
riem-pham <riem-pham@kitting-users-MacBook-Pro.local>
2019-07-25 16:51:41 +0700

    adding message

--- src/config/key.js
+++ src/config/key.js
@@ -2,5 +2,6 @@ export const keys = {
   braintree_mearchIdKey: "8zvhvjf3qfvnbm84",
   braintree_publicKey: "vmtsh3f8rn49j4jt",
   braintree_privateKey: "8993fc128149e3b18f9a0def5119f8a0",
-  db: "mongodb://localhost:27017/ecommerce"
+  db:
+    "mongodb+srv://riemdata123:testpass123@cluster0-t8ifq.mongodb.net/ecommerce?retryWrites=true&w=majority"
 };
